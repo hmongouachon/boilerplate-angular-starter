3f72563dbfffdecc53c4b623cb0866398100f185
hmongouachon <hadrien.mongouachon@gmail.com>
2017-10-04 15:33:17 +0200

    re order repository

--- dist/min-safe/about_ctrl.js
+++
@@ -1,8 +0,0 @@
-
-app.controller("about_ctrl", [ '$scope',  '$rootScope', '$localStorage',  function($scope, $rootScope,  $localStorage) {  
-
-// awesome code here
-$scope.sayHello = 'This is the about page ... be honest !';
-    
-}]);
-
